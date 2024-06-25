d0b45438a6489b3f65e604bdeedec99cacdba17b
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2024-06-26 03:25:24 +0500

    about-route

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = 10000
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+app.get('/about', (req, res) => {
+    res.send('about request ')
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
